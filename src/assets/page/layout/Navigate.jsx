246ed49dd7e469f3982782362edd5c6f98a7213b
[Muhammed] <[nameismilo2@gmail.com]>
2025-10-23 00:25:40 +0300

    update responsive at navigation

--- src/assets/page/layout/Navigate.jsx
+++ src/assets/page/layout/Navigate.jsx
@@ -9,7 +9,7 @@ const Navigate = () => {
   return (
     <div className={`w-full fixed z-40 ${isOpen && ('top-8')}`}>
       <div className='flex justify-between my-4 bg-white text-center items-center rounded-full px-6 py-2 mx-[7%]'>
-        <div className='flex gap-8'>
+        <div className='flex gap-4 sm:gap-6 md:gap-8'>
           <Link to='shop' className='text-sm font-semibold lg:text-base'>Shop</Link>
           <Link to='about' className='text-sm font-semibold lg:text-base'>About</Link>
         </div>
@@ -17,7 +17,7 @@ const Navigate = () => {
           <img src="/image/black-flower-icon.png" alt="flower-icon" className='w-6 sm:hidden' />
           <img src="/image/black-logo.png" alt="PURFUM" className='sm:w-24 hidden sm:block' />
         </Link>
-        <div className='flex gap-8'>
+        <div className='flex gap-4 sm:gap-6 md:gap-8'>
           <Link to='blog' className='text-sm font-semibold lg:text-base'>Blog</Link>
           <Link to='contact' className='text-sm font-semibold gap-x-8 lg:text-base'>Contact</Link>
           <Link to='cart' className='text-sm font-semibold lg:text-base self-center'><LiaShoppingBagSolid /></Link>
