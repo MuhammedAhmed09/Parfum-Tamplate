495ace6e4a5335b255b82bde607487c3eb023396
[Muhammed] <[nameismilo2@gmail.com]>
2025-10-23 00:36:12 +0300

    test solve error for vercel

--- src/assets/page/outlet/Product.jsx
+++ src/assets/page/outlet/Product.jsx
@@ -1,10 +1,9 @@
 import Featured from '../../productSections/Featured';
 import FeaturedImage from '../../productSections/FeaturedImage';
-import ProductDetails from '../../productSections/productDetails';
+import ProductDetails from '../../productSections/ProductDetails';
 
 const Product = () => {
-
-    
+   
   return (
     <section className='bg-Light-Pink'>
       <ProductDetails />
