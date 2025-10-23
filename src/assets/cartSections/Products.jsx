b2388c6fbafa91ffaa8d6a3fa9731fc49b71fecd
[Muhammed] <[nameismilo2@gmail.com]>
2025-10-23 22:12:14 +0300

    update empty-cart

--- src/assets/cartSections/Products.jsx
+++ src/assets/cartSections/Products.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { ProductsContext } from '../context/CartProducts'
 import { MdDeleteOutline } from 'react-icons/md';
 import QuantityInput from '../components/QuantityInput';
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   const { cartItems, handleRemoveItem } = useContext(ProductsContext);
@@ -10,9 +11,22 @@ const Products = () => {
   return (
     <div className='w-full flex items-center justify-center min-h-[20vh]'>
       {cartItems.length === 0 ? (
-        <button>YOUR CART IS EMPTY</button>
+        <div className='text-Green font-semibold text-sm md:text-base lg:text-lg flex flex-col gap-4 items-center '>
+          <h3>Your Shopping Cart is Empty</h3>
+          <Link to='/shop' className='border border-Green rounded-full cursor-pointer'>
+            <button className='px-6 py-1 hover:scale-105 duration-300 transition-transform cursor-pointer'>Buy Now</button>
+          </Link>
+        </div>
       ) : (
-        <div className='w-full px-6'>
+        <div className='w-full px-6 grid gap-y-8'>
+          <div className='font-semibold text-sm md:text-base lg:text-lg flex flex-row justify-between gap-4 items-center'>
+            <h3>Shopping Cart</h3>
+            <Link to='/shop' className='hidden sm:flex border-b cursor-pointer'>
+              <button className='px-4 md:px-6 py-1 hover:scale-105 duration-300 transition-transform cursor-pointer'>
+                continue shopping
+              </button>
+            </Link>
+          </div>
           <div className='grid grid-cols-5 justify-between w-full font-semibold text-sm md:text-base text-left border-b border-black/30 mb-2'>
             <h3 className='col-span-2'>PRODUCT</h3>
             <h3>PRICE</h3>
--- src/assets/page/outlet/Cart.jsx
+++ src/assets/page/outlet/Cart.jsx
@@ -4,7 +4,6 @@ import Products from '../../cartSections/Products'
 const Cart = () => {
   return (
     <section className='bg-Light-Pink w-full flex flex-col gap-4 pt-28 items-center justify-center'>
-      <h3 className='text-xl font-semibold sm:text-2xl md:text-3xl'>YOUR CART</h3>
       <Products />
     </section>
   )
